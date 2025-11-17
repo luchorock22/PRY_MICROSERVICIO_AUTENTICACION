04cc0a50d2ac0c2cef57775c4074c0be65cabfae
Kelly Isabel Altamirano Abata <kialtamirano1@espe.edu.ec>
2025-11-17 16:35:28 -0500

    Refactor middleware configuration and add exception handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,13 +7,13 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-
-        // Alias del middleware de roles
-        $middleware->alias('role', \App\Http\Middleware\CheckRole::class);
-
+    ->withMiddleware(function (Middleware $middleware): void {
+        //
     })
-    ->create();
+    ->withExceptions(function (Exceptions $exceptions): void {
+        //
+    })->create();
